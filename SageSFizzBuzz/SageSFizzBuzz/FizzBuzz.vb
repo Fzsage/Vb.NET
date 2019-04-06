62101f784607146e0de18bc8f5d172bba318efe1
Sage Shoemake <sage.shoemake@yahoo.com>
2019-04-06 15:21:47 -0400

    VB.NET
    VB.NET

---
+++ SageSFizzBuzz/SageSFizzBuzz/FizzBuzz.vb
@@ -0,0 +1,39 @@
+﻿'-by Sage Shoemake
+'This is my implementation of the popular coding challenge "FizzBuzz". 
+'It will loop through integers 1 through 100
+'If the number is divisible by 3, it will output "Fizz" to the console
+'If the number is divisible by 5, it will output "Buzz" to the console
+'If the number is divisible by 3 and 5 (or 15), it will output "FizzBuzz" to the console
+'If a number doesn't match any of these scenarios, it alone will be output to the console
+
+Module FizzBuzz
+
+	Sub Main()
+
+		For i As Integer = 1 To 100
+			fizzBuzzIt(i) 'pass iterations of integer loop to subroutine to keep Main clean
+		Next
+
+		Console.WriteLine("Press any key to close") 'so console doesn't close immediately
+		Console.ReadKey()
+
+	End Sub
+
+	Sub fizzBuzzIt(ByVal i As Integer)
+
+		If (i Mod 15) = 0 Then  'if divisible by 3 and 5, FizzBuzz. Must be first, or else you'll never see a FizzBuzz
+			Console.WriteLine("FizzBuzz!")
+
+		ElseIf (i Mod 3) = 0 Then 'if divisible by 3, then Fizz
+			Console.WriteLine("Fizz!")
+
+		ElseIf (i Mod 5) = 0 Then 'if divisble by 5, then Buzz
+			Console.WriteLine("Buzz!")
+
+		Else
+			Console.WriteLine(i) 'for all other cases when number is not divisible by 3 or 5 (or both)
+		End If
+
+	End Sub
+
+End Module
---
+++ SageSWriteToJSON/SageSWriteToJSON/WriteToJSON.vb
@@ -0,0 +1,91 @@
+﻿'-by Sage Shoemake
+'	This console app will create a class With multiple properties, create instances Of that class in a list,
+'serialize and Write the list as JSON to a text file, and finally deserialize and read the list before writing 
+'it to the console
+
+Imports System.IO
+Imports Newtonsoft.Json
+
+Module WriteToJSON
+
+	Sub Main()
+		Dim list = GetDogs() 'create a list of dogs
+		Dim path As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\dogs.txt"
+
+		WriteToJSON(list) 'pass created list to subroutine to serialize and write JSON to a text file
+		ReadFromJson(path) 'pass file path created above to subroutine to deserialize and read JSON we created earlier, and write it to console
+
+		Console.WriteLine("Press any key to close") 'so console doesn't immediately close
+		Console.ReadKey()
+	End Sub
+
+	'class that we'll be using to create a list of objects to write to JSON 
+
+	Public Class Dog
+        Public Property DogName As String
+        Public Property Gender As String
+        Public Property Breed As String
+        Public Property Color As String
+
+        Public Sub New()
+
+        End Sub
+
+		'Constructor
+
+		Public Sub New(ByVal name As String,
+					   ByVal gndr As String,
+					   ByVal brd As String,
+					   ByVal clr As String)
+			DogName = name
+			Gender = gndr
+			Breed = brd
+			Color = clr
+		End Sub
+
+		'to write the objects and its respective properties to console
+
+		Public Sub Print()
+			Console.WriteLine("Dog Name: " & DogName & ", Breed: " & Breed & ", Gender: " & Gender & ", Color: " & Color)
+			Console.WriteLine()
+		End Sub
+	End Class
+
+	'will create new instances of Dog class and add them to a list
+
+	Function GetDogs() As IEnumerable(Of Dog)
+        Return New List(Of Dog) From
+            {
+            New Dog("Hershey", "Male", "Rhodesian Ridgeback", "Brown"),
+            New Dog("Chance", "Male", "Rat Terrier", "White"),
+            New Dog("Luna", "Female", "Australian Shepherd", "Grey/Brown")
+            }
+    End Function
+
+	'takes Dog class as arg, creates a writable file path, serializes the collection of Dogs into JSON, and writes a JSON string to text file
+
+	Sub WriteToJSON(ByVal dogs As IEnumerable(Of Dog))
+
+		Dim path As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\dogs.txt"
+
+		File.WriteAllText(path, JsonConvert.SerializeObject(dogs, Newtonsoft.Json.Formatting.Indented))
+
+	End Sub
+
+	'takes a file path as arg, reads the file to a string, deserializes the JSON string into a collection of Dog objects,
+	'and prints the objects and their properties to the console using Dog class' Print subroutine
+
+	Sub ReadFromJson(ByVal jsonFile As String)
+
+		Dim readText As String = File.ReadAllText(jsonFile)
+		Dim newDogs As List(Of Dog) = JsonConvert.DeserializeObject(Of List(Of Dog))(readText)
+
+		For Each d In newDogs
+
+			d.Print()
+
+		Next
+
+	End Sub
+
+End Module
